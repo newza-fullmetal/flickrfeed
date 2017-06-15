9dbfb0f54601d9b1aa0a7aec9a4c84a152d1d422
Swish Live <Swish@MacBook-Pro-de-Alexandre-2.local>
2017-06-15 14:33:51 +0200

    Initial Commit

---
+++ Flicker Feed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flicker Feed
+//
+//  Created by Swish Live  on 15/06/2017.
+//  Copyright © 2017 Swish Live. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
